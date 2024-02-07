b250fe95b07b90ca12a0d42f55e5b8b00ce42a07
дмитрий клочков <d.klochkov9421@gmail.com>
2024-02-08 01:36:15 +0300

    дз 2

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -6,3 +6,42 @@ from django.db import models
 class MyModel(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
+
+class Customer(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=20)
+    address = models.TextField()
+    registration_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    photos = models.PositiveIntegerField(default=20)
+    videos = models.PositiveIntegerField(default=0)
+    reels = models.PositiveIntegerField(default=1)
+    
+    def __str__(self):
+        return self.title
+    
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='orders')
+    products = models.ManyToManyField(Product, through='OrderProduct')
+    total_sum = models.DecimalField(max_digits=10, decimal_places=2)
+    order_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Order {self.id} by {self.customer}"
+    
+class OrderProduct(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product} for {self.order}"
+    
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 import logging
 from django.shortcuts import render
 from django.templatetags.static import static
-
+from .models import Product
 
 # Create your views here.
 # Настраиваем логирование
@@ -16,3 +16,7 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def product_list(request):
+    products = Product.objects.all()  # Извлекаем все продукты
+    return render(request, 'myapp/product_list.html', {'products': products})
